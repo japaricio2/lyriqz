9a9a282e7a6bd050247da1082fff001df367e9a6
Jose <japaric4@calstatela.edu>
2018-07-31 21:58:08 -0700

    update path join directory

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(connectFlash());
 app.use(require('express-session')({
   secret: 'ubuntu is a big red horse',
