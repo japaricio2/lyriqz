b445221f54ea0a2c9e0205cb3c771d2ff1a137af
Jose <japaric4@calstatela.edu>
2018-08-24 17:47:50 -0700

    views dir

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ passport.use(new SpotifyStrategy({
 var port = 3000;
 var app = express();
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
